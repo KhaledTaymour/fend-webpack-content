4b52e48347b7b4666c4440172d6b82b3078c9002
K_Taymour <khaled.taymour@esrinea.com>
2021-03-19 15:24:29 +0200

    [feat]: added BundleAnalyzerPlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "production",
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: "./src/client/index.js",
   module: {
     rules: [
@@ -29,5 +32,15 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new BundleAnalyzerPlugin(),
   ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,10 @@ const path = require("path");
 const webpack = require("webpack");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+  
 module.exports = {
   mode: "development",
   entry: "./src/client/index.js",
@@ -28,5 +31,15 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+    new BundleAnalyzerPlugin(),
   ],
 };

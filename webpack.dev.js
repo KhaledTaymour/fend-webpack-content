c4e65e92f6256f25e8467240a4c188f7fcb3fc6f
K_Taymour <khaled.taymour@esrinea.com>
2021-03-20 06:12:29 +0200

    [feat]: added sass loader, fixes devServer on 9000

--- webpack.dev.js
+++ webpack.dev.js
@@ -25,6 +25,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
@@ -32,15 +36,10 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
-    new BundleAnalyzerPlugin(),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    open: true,
+  },
 };
